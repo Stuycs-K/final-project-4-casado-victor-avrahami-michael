d058c0f7d225edf1ae1862c06f09264e6ed1d4ab
Victor Casado <vcasado50@stuy.edu>
2024-05-22 15:32:15 -0400

    GamePiece class

---
+++ GamePiece.java
@@ -0,0 +1,23 @@
+public abstract class GamePiece{
+    //public String name; Idk what its used for tho
+    private int type;
+    public int xLoc, yLoc;
+    public boolean OwnedByPlayerOne;
+
+    public GamePiece(int type, int x, int y, boolean isPlayerOneTurn){
+        this.type = type;
+        xLoc = x;
+        yLoc = y;
+        OwnedByPlayerOne = isPlayerOneTurn;
+    }
+
+    abstract boolean move(int newX, int newY);
+    abstract boolean isLegalMove(int newX, int newY);
+    public boolean canMove(){
+        return true;//placeholder
+    }
+    public String toString(){
+        return "";//placeholder
+    }
+
+}
