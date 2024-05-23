082e3cb8c311922cc91ae5509be7671454ef685e
Victor Casado <vcasado50@stuy.edu>
2024-05-23 10:37:41 -0400

    start of game class

---
+++ Game.java
@@ -0,0 +1,6 @@
+public class Game{
+    public GamePiece[][] board;
+    public boolean isPlayerOneTurn;
+    public GamePiece[] player1Pieces, player2Pieces;
+    public Queen player1Queen, player2Queen;
+}
