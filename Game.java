aba1797df557e468e9cedfe36a9deef673d1ebf9
Victor Casado <vcasado50@stuy.edu>
2024-05-23 10:48:03 -0400

    getNeighbors and constructor

--- Game.java
+++ Game.java
@@ -3,4 +3,21 @@ public class Game{
     public boolean isPlayerOneTurn;
     public GamePiece[] player1Pieces, player2Pieces;
     public Queen player1Queen, player2Queen;
+
+    public Game(){
+        player1Pieces = new GamePiece[11];
+        player2Pieces = new GamePiece[11];
+        //create queens when added into game
+        isPlayerOneTurn = true;
+        board = new GamePiece[22][22];
+    }
+
+    public GamePiece[] getNeighbors(int x, int y){
+        if(y % 2 == 0){
+            return new GamePiece[] {board[x-1][y], board[x][y-1], board[x][y+1], board[x+1][y], board[x-1][y-1], board[x-1][x+1]};
+        }
+        else{
+            return new GamePiece[] {board[x-1][y], board[x][y-1], board[x][y+1], board[x+1][y], board[x+1][y-1], board[x+1][x+1]};
+        }
+    }
 }
