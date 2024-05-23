4d9c8d7d27ac05ac3e1ed5ee1c8214d95520b14f
Victor Casado <vcasado50@stuy.edu>
2024-05-23 11:03:12 -0400

    testing whether queens are trapped works

--- Game.java
+++ Game.java
@@ -12,6 +12,17 @@ public class Game{
         board = new GamePiece[22][22];
     }
 
+    public void setQueen(Queen queen){
+        if(isPlayerOneTurn)
+            player1Queen = queen;
+        else
+            player2Queen = queen;
+    }
+
+    public void toggleTurn(){
+        isPlayerOneTurn = !isPlayerOneTurn;
+    }
+
     public GamePiece[] getNeighbors(int x, int y){
         if(y % 2 == 0){
             return new GamePiece[] {board[x-1][y], board[x][y-1], board[x][y+1], board[x+1][y], board[x-1][y-1], board[x-1][x+1]};
@@ -22,8 +33,8 @@ public class Game{
     }
 
     public int isGameOver(){//0 for false, 1 for p1 queen trapped, 2 for p2 queen trapped, 3 for both trapped
-        GamePiece[] queen1Neighbors = getNeighbors(player1Queen.getX(), player1Queen.get(Y));
-        GamePiece[] queen2Neighbors = getNeighbors(player2Queen.getX(), player2Queen.get(Y));
+        GamePiece[] queen1Neighbors = getNeighbors(player1Queen.getX(), player1Queen.getY());
+        GamePiece[] queen2Neighbors = getNeighbors(player2Queen.getX(), player2Queen.getY());
         boolean queen1Trapped = true;
         for(int i = 0; i < queen1Neighbors.length; i++){
             if(queen1Neighbors[i] == null)
